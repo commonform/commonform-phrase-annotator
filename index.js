2e6f38f36db6a73e21b40ab19756dd0cccc72f9f
Kyle Mitchell <kyle@kemitchell.com>
2015-09-09 00:40:24 -0700

    Use escape-regexp

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 var reAnnotator = require('commonform-regexp-annotator')
-
-var rePattern = /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g
-var reReplace = '\\$&'
+var escape = require('escape-regexp')
 
 function commonformPhraseAnnotator(phrases, annotator) {
   return reAnnotator(
     phrases
       .map(function(phrase) {
         return new RegExp(
-          ( '\\b(' + phrase.replace(rePattern, reReplace) + ')\\b' ),
+          ( '\\b(' + escape(phrase) + ')\\b' ),
           'i') }),
     function(form, path, expression, match) {
       return annotator(form, path, match[1]) }) }
